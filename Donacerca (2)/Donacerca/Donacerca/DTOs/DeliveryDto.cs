77ade4465eb2cb108a417115990ed87bed59fb7a
Stephanie Najera <stephanie_najera@unitec.edu>
2026-06-20 08:12:26 -0600

    Remover carpeta duplicada Donacerca (2) de la rama

--- Donacerca (2)/Donacerca/Donacerca/DTOs/DeliveryDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Donacerca.DTOs;
-
-public class CreateDeliveryDto
-{
-    public string PostId { get; set; } = string.Empty;
-    public DateTime DeliveryDate { get; set; }
-    public string DeliveryLocation { get; set; } = string.Empty;
-}
